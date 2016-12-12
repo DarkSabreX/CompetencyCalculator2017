18d7c75d8b52cc1adf2e5f4f26c08f57a67c1416
Remy Z Romano <The.IronFist.Studio@gmail.com>
2016-12-12 17:51:47 -0600

    Updated Package Json to include all dependencies. Updated server.js to make express point to files relative to their final locations for ease of coding.

--- js/calc.js
+++ js/calc.js
@@ -1,98 +1,98 @@
 // JavaScript File
 (function(){
-                var app = angular.module('calc', ['ngMaterial', 'md.data.table']);
-                
-                app.config(function($mdThemingProvider) {
-                    $mdThemingProvider.theme('default')
-                      .warnPalette('deep-purple');
-                      
-                    $mdThemingProvider.theme('1')
-                      .primaryPalette('red');
-                    
-                    $mdThemingProvider.theme('2')
-                      .primaryPalette('amber');
-                      
-                    $mdThemingProvider.theme('3')
-                      .primaryPalette('green');
-                      
-                    $mdThemingProvider.theme('4')
-                      .primaryPalette('teal');  
-                    
-                    $mdThemingProvider.theme('5')
-                      .primaryPalette('indigo');
-                    
-                    $mdThemingProvider.alwaysWatchTheme(true);
-                });
-                
-                app.controller('CompMatrixController', function(){
-                    this.comptncs = comparr;
-                    this.comp = {};
-                    this.compsel = {};
-                    this.comptbl = {show: false};
-                    this.results = {show: false};
-                    this.ratings = [
-                        {
-                            id: "1",
-                            title: "Did Not Meet Expectations",
-                            style: 'text-align:left; padding-left: 8px;',
+				var app = angular.module('calc', ['ngMaterial', 'md.data.table']);
+				
+				app.config(function($mdThemingProvider) {
+					$mdThemingProvider.theme('default')
+					  .warnPalette('deep-purple');
+					  
+					$mdThemingProvider.theme('1')
+					  .primaryPalette('red');
+					
+					$mdThemingProvider.theme('2')
+					  .primaryPalette('amber');
+					  
+					$mdThemingProvider.theme('3')
+					  .primaryPalette('green');
+					  
+					$mdThemingProvider.theme('4')
+					  .primaryPalette('teal');  
+					
+					$mdThemingProvider.theme('5')
+					  .primaryPalette('indigo');
+					
+					$mdThemingProvider.alwaysWatchTheme(true);
+				});
+				
+				app.controller('CompMatrixController', function(){
+					this.comptncs = comparr;
+					this.comp = {};
+					this.compsel = {};
+					this.comptbl = {show: false};
+					this.results = {show: false};
+					this.ratings = [
+						{
+							id: "1",
+							title: "Did Not Meet Expectations",
+							style: 'text-align:left; padding-left: 8px;',
 							color: 'color: #F44336;'
-                        },
-                        {
-                            id: "2",
-                            title: "Partially Met Expectations",
-                            style: 'text-align:left;',
+						},
+						{
+							id: "2",
+							title: "Partially Met Expectations",
+							style: 'text-align:left;',
 							color: 'color: #ff6f00;'
-                        },
-                        {
-                            id: "3",
-                            title: "Fully Met Expectations",
-                            style: 'text-align: center;',
+						},
+						{
+							id: "3",
+							title: "Fully Met Expectations",
+							style: 'text-align: center;',
 							color: 'color: #4CAF50;'
-                        },
-                        {
-                            id: "4",
-                            title: "Exceeded Expectations",
-                            style: 'text-align:right;',
+						},
+						{
+							id: "4",
+							title: "Exceeded Expectations",
+							style: 'text-align:right;',
 							color: 'color: #009688;'
-                        },
-                        {
-                            id: "5",
-                            title: "Distinguished Performance",
-                            style: 'text-align:right; white-space:break-word; padding-right:5px;',
-                            color: 'color: #3F51B5;'
-                        }
-                    ];
-                    this.comptxt = [];
-                    this.comptotal = 0;
-                    this.comptotaltxt = {};
+						},
+						{
+							id: "5",
+							title: "Distinguished Performance",
+							style: 'text-align:right; white-space:break-word; padding-right:5px;',
+							color: 'color: #3F51B5;'
+						}
+					];
+					this.comptxt = [];
+					this.comptotal = 0;
+					this.comptotaltxt = {};
 					this.comptotaltxtcolor = {};
-                    
-                    this.compscore = function(){
-                        this.results.show = true;
-                        var compsubtotal = 0;
-                        for (var i=0;i<this.compsel.rows.length;i++){
-                            var tmpCell = 0;
-                            tmpCell = Math.round(parseInt(this.compsel.rows[i].curval, 10));
-                            this.comptxt.push(this.compsel.rows[i].behvs[tmpCell-1].desc);
-                            compsubtotal += this.compsel.rows[i].curval;
-                        }
-                        this.comptotal = Math.round(parseFloat(compsubtotal) / parseFloat(this.compsel.rows.length));
-                        this.comptotaltxt = this.ratings[this.comptotal-1].title;
+					
+					this.compscore = function(){
+						this.results.show = true;
+						var compsubtotal = 0;
+						for (var i=0;i<this.compsel.rows.length;i++){
+							var tmpCell = 0;
+							tmpCell = Math.round(parseInt(this.compsel.rows[i].curval, 10));
+							this.comptxt.push(this.compsel.rows[i].behvs[tmpCell-1].desc);
+							compsubtotal += this.compsel.rows[i].curval;
+						}
+						this.comptotal = Math.round(parseFloat(compsubtotal) / parseFloat(this.compsel.rows.length));
+						this.comptotaltxt = this.ratings[this.comptotal-1].title;
 						this.comptotaltxtcolor = this.ratings[this.comptotal-1].color;
-                    };
-                    
-                    this.reloadcalc = function(){
-                        window.location.reload();
-                    };
-                    
-                    this.closecalc = function (){
-                        window.close();
-                    };
-                });
+					};
+					
+					this.reloadcalc = function(){
+						window.location.reload();
+					};
+					
+					this.closecalc = function (){
+						window.close();
+					};
+				});
 
-                //this loader init code must be included
-                angular.element(document).ready(function() {
-                angular.bootstrap(document, ['calc', 'ngMaterial', 'md.data.table']);
-                    });
+				//this loader init code must be included
+				angular.element(document).ready(function() {
+				angular.bootstrap(document, ['calc', 'ngMaterial', 'md.data.table']);
+					});
 
-            })();
+			})();
---
+++ server.js
@@ -0,0 +1,38 @@
+﻿//node module requires for web and websockets server
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+var serverPort = '3000';
+
+app.use(express.static(__dirname + '/')); 	// set the static files location /img will be /img for users
+
+app.use('/content/AFNI/publications/2716/wor/wo_4514/css', express.static(__dirname + '/node_modules/angular-material'));     // Angular Material CSS
+app.use('/content/AFNI/publications/2716/wor/wo_4514/css', express.static(__dirname + '/node_modules/angular-material-data-table/dist'));     // MD Data Table CSS
+app.use('/content/AFNI/publications/2718/wor/wo_4525/css', express.static(__dirname + '/css'));     // MD Data Table CSS
+
+
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular'));     // set static files location for angular js files
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular-aria'));     // set static files location for angular animate
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular-animate'));     // set static files location for angular animate
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular-messages'));     // set static files location for angular messages
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular-material'));     // set static files location for angular material
+app.use('/content/AFNI/publications/2716/wor/wo_4514/js', express.static(__dirname + '/node_modules/angular-material-data-table/dist'));     // MD Data Table js
+
+app.use('/content/AFNI/publications/2718/wor/wo_4525/js', express.static(__dirname + '/js'));     // Calculator and comparray JS
+
+//app.use('/angular', express.static(__dirname + '/node_modules/svg-morpheus/compile/unminified'));
+//app.use('/angular', express.static(__dirname + '/node_modules/v-accordion/dist'));
+//app.use('/angular', express.static(__dirname + '/node_modules/angular-ui-router/release'));
+//app.use('/d3', express.static(__dirname + '/node_modules/d3/build'));
+
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
+
+var server = require('http').Server(app);
+
+//Start the Express Server
+server.listen(serverPort, function () {
+    console.log('Simple static server listening on port ' + serverPort);
+});
