aba32e0f8784859cf7a99dd7ee3802c7d93f9579
Remy Z Romano <The.IronFist.Studio@gmail.com>
2016-12-19 13:04:51 -0600

    Further componentization of items. Separated template code into separate files for easier styling.

--- app/components/matrixResults.js
+++ app/components/matrixResults.js
@@ -8,38 +8,5 @@ angular.module('calc')
                 window.location.reload(true);
             };
         },
-        template:
-           
-          ` <md-content> 
-            <md-card>
-                {{$ctrl.competency.name}}
-                <md-card-header>
-                        <md-card-header-text>
-                            <span class="md-headline">Step 2</span>
-                        </md-card-header-text>
-                    </md-card-header>
-            </md-card>
-            <md-card class="tblcard">
-                    
-                    <md-card-content>
-                        <div style="text-align: center;" style="margin-bottom: 2%;">
-                            <span>The Rating is:</span>
-                            <h2 ng-attr-style="{{cmatrix.comptotaltxtcolor}}">{{$ctrl.finalScore}}</h2> or <h2 ng-attr-style="{{cmatrix.comptotaltxtcolor}}">{{cmatrix.comptotaltxt}}</h2>
-                            <span>Copy the text below. Enter the rating into the <b>{{cmatrix.compsel.name}}</b> Section and paste the behavior list into the comments section.</span>
-                            <br />
-                            <i class="material-icons" style="font-size: xx-large; color: black;">&#xE5DB;</i>
-                        </div>
-                        <div class="md-whiteframe-4dp" style="padding: 2%;">
-                            <span class="layout-max-width" ng-repeat="object in $ctrl.finalDescriptions"><b>{{object.competency}}</b>: {{object.description}}</span>
-                        </div>
-                    </md-card-content>
-                </md-card>
-            </md-content>
-            <md-card>
-                    <md-card-actions layout="row" layout-align="space-between center" layout-wrap layout-margin layout-padding>
-                        <md-button class="md-raised md-warn" ng-click="$ctrl.reloadcalc()">Start Over</md-button>
-                    </md-card-actions>
-                </md-card>
-         `
-         
+        templateUrl: 'app/pages/MatrixResults.html'       
     });
--- app/components/pagecontainer.js
+++ app/components/pagecontainer.js
@@ -3,11 +3,5 @@
 angular.module('calc')
     .component('pagecontainer', {
         bindings: { competencies: '<' },
-        template:
-        `
-         <md-toolbar>ALPS Competency Calculator</md-toolbar>
-         <div layout="column" layout-fill>
-            <ui-view></ui-view>
-         </div>
-        `
+        templateUrl: 'app/pages/PageContainer.html'       
     });
